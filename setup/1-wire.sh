655867e485462a943e1bd6ad77e3ed02c83a135a
snjallingur <snjallingur@snjallingur.is>
2022-11-17 11:48:38 +0000

    Create 1-wire.sh

---
+++ setup/1-wire.sh
@@ -0,0 +1,6 @@
+#setup of the OWserver on Ubuntu
+#Guideline: https://owfs.org/index_php_page_quickstart-guide.html
+sudo apt-get install owserver owhttpd owfs
+
+#1-wire usb dongle
+#https://denkovi.com/usb-to-one-wire-interface-adaptor-converter-thermometer
